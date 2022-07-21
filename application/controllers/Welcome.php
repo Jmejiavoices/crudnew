2f5bd875e8b7e4a262157e56b0293fe00fd36b7a
Jmejiavoices <jmejia@voices.com.mx>
2022-07-21 16:46:28 -0500

    Actualizacion FORM Y GUARDAR BASE DE DATOS

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,61 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper(array('form', 'url'));
+
+		$this->load->library('form_validation');
+
+		$this->load->model('crud_model');
+	}
+
+
+
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+
+
+
+
+
+
+	public function insert()
+	{
+		if ($this->input->is_ajax_request()) {
+			$this->form_validation->set_rules('name', 'name', 'required');
+			$this->form_validation->set_rules('ap', 'ap', 'required');
+			$this->form_validation->set_rules('am', 'am', 'required');
+			$this->form_validation->set_rules('edad', 'edad', 'required');
+			$this->form_validation->set_rules('email', 'email', 'required|valid_email');
+			if ($this->form_validation->run() == FALSE) {
+				$data = array('responce' => 'error', 'message' => validation_errors());
+			}
+			 else
+			    {
+
+				$ajax_data =  $this->input->post();
+				if ($this->crud_model->insert_entry($ajax_data)) {
+				
+					$data = array('responce' => 'success', 'message' => 'Usuaio Agregado con Éxito');
+				}
+				else {
+					$data = array('responce' => 'error', 'message' => 'No se Agrego el Usuario');
+				}
+
+
+				}
+			echo json_encode($data);
+		} else {
+			echo "No direct script access allowed";
+		}
 	}
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,100 +1,131 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<style type="text/css">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
-
-	a:hover {
-		color: #97310e;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
-
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<title>Hello, world!</title>
 </head>
+
 <body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 mt-5">
+				<h1 class="text-center">
+					Usuarios
+				</h1>
+				<hr style="background-color: black; color: black; height: 1px;">
+				</hr>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 mt-2">
+				<!-- AÑADIR REGISTROS 	-->
+				<!-- Button trigger modal -->
+				<button type="button" class="btn btn-outline-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+					Agregar
+				</button>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+				<!-- Modal -->
+				<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Registra tus datos</h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body">
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+								<form action="" method="post">
+									<div class="form-group">
+										<label for="">Nombre</label>
+										<input type="text" id="name" class="form-control">
+									</div>
+									<div class="form-group">
+										<label for="">Apellido Paterno</label>
+										<input type="text" id="ap" class="form-control">
+									</div>
+									<div class="form-group">
+										<label for="">Apellido Materno</label>
+										<input type="text" id="am" class="form-control">
+									</div>
+									<div class="form-group">
+										<label for="">Edad</label>
+										<input type="text" id="edad" class="form-control">
+									</div>
+									<div class="form-group">
+										<label for="">Correo</label>
+										<input type="email" id="email" class="form-control">
+									</div>
+								</form>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Salir</button>
+								<button type="button" class="btn btn-primary" id="add">Guardar</button>
+							</div>
+						</div>
+					</div>
+				</div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are explorinG CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
+			</div>
+		</div>
 	</div>
+	<!-- Optional JavaScript; choose one of the two! -->
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	<!-- Option 2: Separate Popper and Bootstrap JS -->
+
+	<!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> -->
+	<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> -->
+
+	<!-- AÑADIR DATOS -->
+	<script>
+		$(document).on("click", "#add", function(e) {
+			e.preventDefault();
+
+			var name = $("#name").val();
+			var ap = $("#ap").val();
+			var am = $("#am").val();
+			var edad = $("#edad").val();
+			var email = $("#email").val();
+
+			if (name == "" || ap == "" || am == "" || edad == "" || email == "") {
+				alert("Todos los datos son necesarios.")
+			} else {
+				$.ajax({
+					url: "<?php echo base_url(); ?>insert",
+					type: "post",
+					dataType: "json", 
+					data: {
+						name: name,
+						ap: ap,
+						am: am,
+						edad: edad,
+						email: email,
+
+					},
+					success: function(data) {
+						console.log(data);
+					}
+
+
+
+				});
+			}
+
+		});
+	</script>
+
 
 </body>
+
 </html>
